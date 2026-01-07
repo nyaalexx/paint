2f272293b5ae1658199d2f333118f8c0779d67cb
Alex <nyaalex@proton.me>
2026-01-07 17:27:54 +0300

    Add target guards in android

--- crates/paint-android/src/lib.rs
+++ crates/paint-android/src/lib.rs
@@ -1,6 +1,11 @@
+#[cfg(target_os = "android")]
 mod gpu;
 
+#[cfg(target_os = "android")]
 pub mod behaviour;
+#[cfg(target_os = "android")]
 pub mod logging;
+#[cfg(target_os = "android")]
 pub mod renderer;
+#[cfg(target_os = "android")]
 pub mod surface;
