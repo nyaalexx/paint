ffd0becf9ed35bd9175fe4f0cce8792b3a47884a
Alex <nyaalex@proton.me>
2026-01-21 22:37:56 +0300

    Slight tweaks of color picker UI

--- android-app/app/src/main/java/site/nyaalex/paint/ui/color_picker/ColorPicker.kt
+++ android-app/app/src/main/java/site/nyaalex/paint/ui/color_picker/ColorPicker.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import site.nyaalex.paint.ui.color_picker.mode.OkhsvColorPicker
@@ -19,14 +18,13 @@ import site.nyaalex.paint.ui.theme.AppTheme
 fun ColorPicker(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier
-            .background(color = AppTheme.colors.popupBackground.copy(alpha = 0.5f), shape = RoundedCornerShape(16.dp))
+            .background(color = AppTheme.colors.popupBackground.copy(alpha = 0.95f), shape = RoundedCornerShape(16.dp))
             .border(
                 width = 1.dp,
                 color = AppTheme.colors.popupBorder,
                 shape = RoundedCornerShape(16.dp)
             )
             .padding(16.dp)
-            .shadow(elevation = 16.dp)
     ) {
         val textStyle = TextStyle(
             color = AppTheme.colors.text,
